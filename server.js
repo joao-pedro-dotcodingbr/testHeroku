4681c1a91fdfaed8ca382cdcddff40f7628ff122
João Pedro <68718789+joaopedro29@users.noreply.github.com>
2022-02-06 16:43:59 -0300

    Fix: teste os

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const UserAgent = require("user-agents")
 const userAgent = new UserAgent();
 var  s  = new Sniffr(); 
 s.sniff(userAgent.toString())
-
+const agentExpress = require('express-useragent')
 const express = require('express')
 const app = express();
 app.use(express.json());
+app.use(agentExpress.express());
 
 app.get('/', (req, res, next) => {
     try {
      
-        res.status(200).json(s.os)
+        res.status(200).json(req.useragent.isMobile)
     } catch (error) {
         next(error)
     }
